0d9dbbe59189d24a9967d3c63fbc661289c91e93
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-06-26 15:18:39 +0200

    Add click handler

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Drawing;
+using System.IO.Abstractions;
+using System.Windows;
 
 namespace SpecLogLogoReplacer.UI
 {
   public partial class MainWindow
   {
+    private readonly IFileSystem fileSystem;
+
     public MainWindow()
     {
       InitializeComponent();
+
+      this.fileSystem = new FileSystem();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+      var pathToSpecLogFile = this.specLogFilePath.Text;
+      var pathToNewLogo = this.newLogoPath.Text;
+
+      var specLogFile = this.fileSystem.File.ReadAllText(pathToSpecLogFile);
+
+      Image newLogo;
+
+      using (var stream = this.fileSystem.File.OpenRead(pathToNewLogo))
+      {
+        newLogo = Image.FromStream(stream);
+      }
+
+      var patchedSpecLogFile = new LogoReplacer().Replace(specLogFile, newLogo);
+
+      this.fileSystem.File.WriteAllText(pathToSpecLogFile, patchedSpecLogFile);
     }
   }
 }
