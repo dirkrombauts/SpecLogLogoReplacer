0262edd0e1c089ccd36486289416fa70b2c3816c
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-06-27 11:19:00 +0200

    Extract method

--- Tests/ViewModel/SpecLogTransformerTests.cs
+++ Tests/ViewModel/SpecLogTransformerTests.cs
@@ -44,13 +44,19 @@ namespace SpecLogLogoReplacer.Tests.ViewModel
     [Test]
     public void Transform_PathToSpecLogFileStartsWithDoubleQuote_ShouldNotThrowArgumentException()
     {
-      var mockFileSystem = new MockFileSystem();
-      mockFileSystem.AddFile(@"c:\speclog.html", new MockFileData("<html />"));
-      mockFileSystem.AddFile(@"c:\logo.png", new MockFileData(LogoReplacer.ConvertTo(Resources.logo, ImageFormat.Png)));
+      var mockFileSystem = CreateFileSystemWithSimpleHtmlAndDefaultLogo();
       var transformer = CreateSpecLogTransformer(mockFileSystem);
 
       Check.ThatCode(() => transformer.Transform(@"""c:\speclog.html", @"c:\logo.png"))
         .DoesNotThrow();
+    }
+
+    private static MockFileSystem CreateFileSystemWithSimpleHtmlAndDefaultLogo()
+    {
+      var mockFileSystem = new MockFileSystem();
+      mockFileSystem.AddFile(@"c:\speclog.html", new MockFileData("<html />"));
+      mockFileSystem.AddFile(@"c:\logo.png", new MockFileData(LogoReplacer.ConvertTo(Resources.logo, ImageFormat.Png)));
+      return mockFileSystem;
     }
 
     private static SpecLogTransformer CreateSpecLogTransformer(MockFileSystem mockFileSystem)
