9fa77f05220ad1d5c2ec51ad6c5a7d4153feab7b
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-06-27 12:01:04 +0200

    Add feature file and first binding

---
+++ Tests/SaveSettings.feature
@@ -0,0 +1,15 @@
+﻿Feature: Save Settings
+
+Scenario: Saving Settings
+  Given I have these settings
+    | Path to SpecLog Html File | Path to Logo |
+    | c:\speclog.html           | c:\logo.png  |
+  When I save the settings
+  Then the settings file contains
+    """
+    <?xml version="1.0" encoding="utf-8"?>
+    <specLogLogoReplacer xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
+      <pathToSpecLogHtmlFile>c:\speclog.html</pathToSpecLogHtmlFile>
+      <pathToSpecLogo>c:\logo.png</pathToSpecLogo>
+    </specLogLogoReplacer>
+    """
---
+++ Tests/SaveSettingsStepDefinitions.cs
@@ -0,0 +1,27 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SpecLogLogoReplacer.Tests
+{
+  [Binding]
+  public class SaveSettingsStepDefinitions
+  {
+    private Settings settings;
+
+    [Given(@"I have these settings")]
+    public void GivenIHaveTheseSettings(Table table)
+    {
+      string pathToSpecLogHtmlFile = table.Rows[0]["Path to SpecLog Html File"];
+      string pathToLogo = table.Rows[0]["Path to Logo"];
+
+      this.settings = new Settings { PathToSpecLogHtmlFile = pathToSpecLogHtmlFile, PathToLogo = pathToLogo };
+    }
+
+  }
+
+  public class Settings
+  {
+    public string PathToSpecLogHtmlFile { get; set; }
+
+    public string PathToLogo { get; set; }
+  }
+}
