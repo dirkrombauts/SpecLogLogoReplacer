3f92a8895d4fd77ef7f13d10e9182932af9e85bb
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-06-27 11:44:57 +0200

    Removing unnecessary comments

--- UI/ViewModel/MainViewModel.cs
+++ UI/ViewModel/MainViewModel.cs
@@ -5,15 +5,6 @@ namespace SpecLogLogoReplacer.UI.ViewModel
 {
     /// <summary>
     /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
@@ -24,15 +15,6 @@ namespace SpecLogLogoReplacer.UI.ViewModel
         /// </summary>
         public MainViewModel()
         {
-          ////if (IsInDesignMode)
-          ////{
-          ////    // Code runs in Blend --> create design time data.
-          ////}
-          ////else
-          ////{
-          ////    // Code runs "for real"
-          ////}
-
           this.specLogTransformer = new SpecLogTransformer();
           this.transformCommand = new RelayCommand(DoTransform);
         }
