f235940eab68d3a43368addf23101be9ccb78b1e
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-06-27 14:10:25 +0200

    Add unit tests for settingsmanager

---
+++ Tests/SettingsManagerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO.Abstractions.TestingHelpers;
+
+using NFluent;
+
+using NUnit.Framework;
+
+using SpecLogLogoReplacer.UI;
+
+namespace SpecLogLogoReplacer.Tests
+{
+  [TestFixture]
+  public class SettingsManagerTests
+  {
+    [Test]
+    public void Constructor_NullFileSystem_ThrowsArgumentNullException()
+    {
+      Check.ThatCode(() => new SettingsManager(null))
+        .Throws<ArgumentNullException>()
+        .WithProperty("ParamName", "fileSystem");
+    }
+
+    [Test]
+    public void LoadSettings_NullPath_ThrowArgumentNullException()
+    {
+      var settingsManager = new SettingsManager(new MockFileSystem());
+
+      Check.ThatCode(() => settingsManager.LoadSettings(null))
+        .Throws<ArgumentNullException>()
+        .WithProperty("ParamName", "path");
+    }
+
+    [Test]
+    public void LoadSettings_WhiteSpacePath_ThrowArgumentNullException()
+    {
+      var settingsManager = new SettingsManager(new MockFileSystem());
+
+      Check.ThatCode(() => settingsManager.LoadSettings(" \t\n\r"))
+        .Throws<ArgumentNullException>()
+        .WithProperty("ParamName", "path");
+    }
+
+    [Test]
+    public void SaveSettings_NullPath_ThrowArgumentNullException()
+    {
+      var settingsManager = new SettingsManager(new MockFileSystem());
+
+      Check.ThatCode(() => settingsManager.SaveSettings(null, null))
+        .Throws<ArgumentNullException>()
+        .WithProperty("ParamName", "path");
+    }
+
+    [Test]
+    public void SaveSettings_WhiteSpacePath_ThrowArgumentNullException()
+    {
+      var settingsManager = new SettingsManager(new MockFileSystem());
+
+      Check.ThatCode(() => settingsManager.SaveSettings(" \t\n\r", null))
+        .Throws<ArgumentNullException>()
+        .WithProperty("ParamName", "path");
+    }
+
+    [Test]
+    public void SaveSettings_NullSettings_ThrowArgumentNullExceptionForSettings()
+    {
+      var settingsManager = new SettingsManager(new MockFileSystem());
+
+      Check.ThatCode(() => settingsManager.SaveSettings(@"c:\settings.xml", null))
+        .Throws<ArgumentNullException>()
+        .WithProperty("ParamName", "settings");
+    }
+  }
+}
--- UI/SettingsManager.cs
+++ UI/SettingsManager.cs
@@ -9,11 +9,16 @@ namespace SpecLogLogoReplacer.UI
 
     public SettingsManager(IFileSystem fileSystem)
     {
+      if (fileSystem == null) throw new ArgumentNullException("fileSystem");
+
       this.fileSystem = fileSystem;
     }
 
     public void SaveSettings(string path, Settings settings)
     {
+      if (string.IsNullOrWhiteSpace(path)) throw new ArgumentNullException("path");
+      if (settings == null) throw new ArgumentNullException("settings");
+
       using (var stream = this.fileSystem.File.OpenWrite(path))
       {
         stream.Serialize<Settings>(settings);
@@ -22,6 +27,8 @@ namespace SpecLogLogoReplacer.UI
 
     public Settings LoadSettings(string path)
     {
+      if (string.IsNullOrWhiteSpace(path)) throw new ArgumentNullException("path");
+
       Settings result;
 
       using (var stream = this.fileSystem.File.OpenRead(path))
